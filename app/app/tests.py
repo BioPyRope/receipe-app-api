20280ab09324107dc2e87af9b914901cc1542813
bill0707ms <bill0704@gmail.com>
2022-07-09 15:03:37 +0800

    My simple django test

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,18 +1,19 @@
 '''
-simple test 
+simple test
 '''
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
     '''Test calc module'''
-    
+
     def test_add_numbers(self):
         '''測試加法功能'''
-        var= {
-            "x" : 1,
-            "y" : 2,
+        var = {
+            "x":1,
+            "y":2,
         }
-        x,y= var.values()
-        res=calc.add(x,y)
+        x,y = var.values()
+        res = calc.add(x,y)
         self.assertEqual(res,3)
