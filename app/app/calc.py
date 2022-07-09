5071252470e21364d8c9d9af531426f3ca772450
bill0707ms <bill0704@gmail.com>
2022-07-09 14:57:59 +0800

    My simple django test

---
+++ app/app/calc.py
@@ -0,0 +1,5 @@
+#caculator function
+
+def add(x,y):
+    '''就是加法'''
+    return x + y
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+'''
+simple test 
+'''
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    '''Test calc module'''
+    
+    def test_add_numbers(self):
+        '''測試加法功能'''
+        var= {
+            "x" : 1,
+            "y" : 2,
+        }
+        x,y= var
+        res=calc.add(x,y)
+        self.assertEqual(res,3)
